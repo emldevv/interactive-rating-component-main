29b2b19c6828b90719d38e4731d3f52ea305a0a1
emldev <103387355+emldevv@users.noreply.github.com>
2022-08-01 21:15:02 +1000

    Initial JScript work
    Grabbed rating number and submit buttons, confirmed click call back functions on console

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('It works!');
+const rating = document.querySelector('.star-rating-panel');
+const ratingButtons = rating.querySelectorAll('.star-rating-panel_btn');
+const submitButton = document.querySelector('.rating-card__submit-btn');
+
+
+function ratingNumberClick(event) {
+    console.log(event);
+}
+
+submitButton.addEventListener('click', function () {
+    console.log('Ya clicked it!');
+});
+
+// rating.addEventListener('click', ratingNumberClick);
+
+ratingButtons.forEach(button => button.addEventListener('click', ratingNumberClick));
