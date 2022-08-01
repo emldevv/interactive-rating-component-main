475801cec4c5f4024f67fbe88378b2654b6e947e
emldev <103387355+emldevv@users.noreply.github.com>
2022-08-02 05:41:36 +1000

    JS and CSS interactivity works
    Completed individual rating btton recognition in JS
    Completed colour changes for hover and focus states on rating numbers and submit button

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const rating = document.querySelector('.star-rating-panel');
-const ratingButtons = rating.querySelectorAll('.star-rating-panel_btn');
+const ratingButtons = rating.querySelectorAll('.star-rating-panel__btn');
 const submitButton = document.querySelector('.rating-card__submit-btn');
+const cards = document.querySelector('.cards');
+const ratingCards = cards.querySelector('[role="card"]')
 
 
 function ratingNumberClick(event) {
-    console.log(event);
+    console.log(event.currentTarget); // Identifies each radio button
 }
 
 submitButton.addEventListener('click', function () {
-    console.log('Ya clicked it!');
-});
+    // Submit button steps
+    // hide both cards
+    
 
-// rating.addEventListener('click', ratingNumberClick);
+    // mark both cards as unselected
+
+
+    // mark the submit button as selected
+
+
+    // Find the tankyou card and show it
+})
 
 ratingButtons.forEach(button => button.addEventListener('click', ratingNumberClick));
+
+
+// Obtaining rating value
+/*
+var ratingValue = 0;
+var valueField = document.querySelector('.star-value');
+
+
+document.querySelectorAll('.star-rating input[name="rating"]').forEach(function(radio){
+   radio.addEventListener('change', function(){
+      ratingValue = document.querySelector('.star-rating input[name="rating"]:checked').value;
+      valueField.innerHTML = "Rating: " + ratingValue;
+   });
+});
+*/
